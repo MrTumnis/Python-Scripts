57814cf9469b11d4f727dd99c8a352e1fde59e49
mrtumnis <thomas.hefferman1@gmail.com>
2024-10-31 17:58:19 -0600

    update column filter

--- QA_QC/analyzer.py
+++ QA_QC/analyzer.py
@@ -12,18 +12,23 @@ from rich import print #***Used for a better looking and colorful output. I.E er
 '''Key words'''
 ws_keys = {'WindSpeed', 'WS', 'windspeed', 'Ws', '_S_'}
 wd_keys = {'WindDirection', 'WD', 'WindDir', 'wind_direction', 'wind_dir', 'Wd', '_D_', '_D1_'}
-filter_keys = {'WVT', 'WVt', 'Wvt', 'wvt', 'WVC', 'WVc', 'Wvc', 'wvc'}
+sr_keys = {'Solar_Radiation', 'solar_radiation', 'Solar_Rad', 'solar_rad', 'SR', 'sr'}
+precip_keys = {'Precipitation', 'precipitation', 'Precip', 'precip'}
+bp_keys = {'Barometric_Pressure', 'barometric_pressure', 'BP', 'bp', 'baro'}
+temp_keys = {'Temperature', 'temperature', 'temp', 'Temp', 'AirTC', 'TC'}
+rh_keys = {'Relative_Humidity', 'relative_humidity', 'RH', 'rh'}
+#filter keys
+vector_keys= {'WVT', 'WVt', 'Wvt', 'wvt', 'WVC', 'WVc', 'Wvc', 'wvc'}
 avg_keys = {'AVG', 'Avg', 'avg'}
 min_keys = {'MIN', 'Min', 'min'}
 max_keys = {'MAX', 'Max', 'max'}
 tot_keys = {'TOT', 'Tot', 'tot'}
-temp_keys = {'Temperature', 'temperature', 'temp', 'Temp', 'AirTC', 'TC'}
-rh_keys = {'Relative_Humidity', 'relative_humidity', 'RH', 'rh'}
 rtd_keys = {'RTD','rtd'}
+two_m_keys = {'2M', '2m'}
+three_m_keys = {'3M', '3m'}
+ten_m_keys = {'10M','10m'}
+twenty_m_keys = {'20M', '20m'}
 delta_keys= {'DeltaT', 'delta'}
-sr_keys = {'Solar_Radiation', 'solar_radiation', 'Solar_Rad', 'solar_rad', 'SR', 'sr'}
-precip_keys = {'Precipitation', 'precipitation', 'Precip', 'precip'}
-bp_keys = {'Barometric_Pressure', 'barometric_pressure', 'BP', 'bp', 'baro'}
 
 '''Wind Compass'''
 north   = [337.6, 22.5]
@@ -53,15 +58,20 @@ def file_read(file_path):
         print(f"Error: The file '{file_path}' is empty.")
     
         
-def col_filter(name_key,filter_key):
+def col_filter(name_key,filter_key,non_key=None):
     df = time_check()
+    headers = [] 
     df_columns = df[[col for col in df.columns if any(keyword in col for keyword in (name_key))]]
-#    df_index = df_columns.columns  
     new_keys = df[[col for col in df.columns if any(keyword in col for keyword in (filter_key))]]
     for column in df_columns:
-        if any(new_key in column for new_key in new_keys):  
-            return column
-
+        if any(new_key in column for new_key in new_keys): 
+           headers.append(column)
+    for header in headers:
+        if non_key is not None and any(non_key in header for non_key in header):   #had to add because temp_check returned BP_TempC as the column
+            headers.remove(header)
+            return(''.join(headers))
+        else:
+            return(''.join(headers))
     return None 
 
 
@@ -71,13 +81,13 @@ def cell_color(df, condition, col, col2=None):
     if col2 is not None and col2 not in df.columns:
         raise ValueError(f"Column '{col2}' not found in the DataFrame.")
 
-    df_style = pd.DataFrame('', index=df.index, columns=df.columns)  # Initialize style DataFrame
+    df_style = pd.DataFrame('', index=df.index, columns=df.columns)  
 
     # Apply coloring based on the condition
     if col2:
-        df_style.loc[condition, [col, col2]] = f'background-color: {yellow}'  # Color for both columns
+        df_style.loc[condition, [col, col2]] = f'background-color: {yellow}' 
     else:
-        df_style.loc[condition, col] = f'background-color: {lblue}'  # Color only for ws_col
+        df_style.loc[condition, col] = f'background-color: {lblue}' 
 
     return df_style
 
@@ -106,26 +116,27 @@ def ws_check(df, ws_col, wd_col):
 
 def ws_test():
     df = file_read(file_path)
-    ws_title = str.strip(col_filter(ws_keys, filter_keys))
-    wd_title = str.strip(col_filter(wd_keys, filter_keys))
+    ws_title = str.strip(col_filter(ws_keys, vector_keys))
+    wd_title = str.strip(col_filter(wd_keys, vector_keys))
 
     condition = ws_check(df, ws_title, wd_title)  # Get the condition from ws_check
     # Apply cell_color based on the condition
     return df.style.apply(lambda x: cell_color(x, condition, col=ws_title, col2=wd_title), axis=None)
 
 
-#check for 0's or extreme negatives. Compare to RTD, if it exists. 
+#check for 0's or extreme negatives. Compare to RTD 2m, if it exists. 
 def temp_check():
     df = file_read(file_path)
-    temp_title = str.strip(col_filter(temp_keys, avg_keys))
-    rtd_title = str.strip(col_filter(rtd_keys, avg_keys))
+    temp_title = str.strip(col_filter(temp_keys, avg_keys, bp_keys))
+    rtd2m_title = str.strip(col_filter(rtd_keys, two_m_keys))
     rh_title = str.strip(col_filter(rh_keys, avg_keys))
     if temp_title in df.columns:
-        print(temp_title)
+        print(rtd2m_title)
     return df
 
 
-
+#def rtd_check():
+#    rtd2m_title = str.strip(col_filter(rtd_keys, twom_keys))
 #def rh_check():
 #def sr_check():
 #def precip_check():
@@ -134,7 +145,7 @@ def temp_check():
 '''Export the QA/QC file'''
 def QAQC_file():
     date = datetime.datetime.now()
-    styled_df = ws_test()
+    styled_df = temp_check()
     styled_df.to_excel(date.strftime("%Y%m%d") + '-' + f"{file_path.strip('.csv')}" + '.xlsx', index=False)
 
 if __name__ == '__main__':
