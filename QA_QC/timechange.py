a0c1a388194532853bae42d28cb4835724141c43
mrtumnis <thomas.hefferman1@gmail.com>
2025-01-03 19:24:45 -0700

    timechange.py update

--- QA_QC/timechange.py
+++ QA_QC/timechange.py
@@ -4,9 +4,19 @@ import sys
 import numpy as np
 import datetime
 import pandas as pd
-from rich import print #***Used for a better looking and colorful output. I.E errors in cli
 from icecream import ic
-from functools import lru_cache
+from rich import print as rprint #***Used for a better looking and colorful output. I.E errors in cli
+from rich.console import Console
+from rich.theme import Theme
+from rich.prompt import Prompt
+
+custom_theme = Theme({
+    "info": "dim cyan",
+    "warning": "magenta",
+    "error": "bold red"
+})
+
+console = Console(theme=custom_theme)
 
 def file_read(file_path):
     try:
@@ -23,7 +33,7 @@ def file_read(file_path):
         return df_new
 
     except Exception as e:
-        print(f"Error occurred: {e}")
+        console.print(f"#1 Error occurred: {e}", style='error')
     return None
 
         
@@ -41,24 +51,24 @@ def time_check():
     one_day = str('1 days 00:00:00')
 
     time = str(df['diff_check'].mode()[0]) 
-
-    if time == fifteen_min:
-        print(f"This is a 15-min file")
-        time = 15 
-    elif time == thirty_min:
-        print(f"This is a 30-min file")
-        time = 30
-    elif time == one_hour:
-        print(f"This is a 60-min file")
-        time = 60
-    elif time == one_day:
-        print(f"This is a Daily file")
-        time = 1440
-    elif time == five_min:
-        print(f"This is a five-min file")
-        time = 5
-    else:
-        print(f"{time} is not a valid time interval")
+    try:
+        if time == fifteen_min:
+            rprint(f"This is a 15-min file")
+            time = 15 
+        elif time == thirty_min:
+            rprint(f"This is a 30-min file")
+            time = 30
+        elif time == one_hour:
+            rprint(f"This is a 60-min file")
+            time = 60
+        elif time == one_day:
+            rprint(f"This is a Daily file")
+            time = 1440
+        elif time == five_min:
+            rprint(f"This is a five-min file")
+            time = 5
+    except:
+        raise Exception (f"{time} is not a valid time interval")
     return time 
 
 '''NEED TO RAISE ERROR FOR NON INT TYPE FOR INPUT'''
@@ -67,26 +77,30 @@ def time_change():
     df = file_read(file_path)
     df.set_index('TIMESTAMP', inplace=True)
     freq = [15, 30, 60, 1440]
-    time = input("What frequency would you like to change the file to? 15, 30, 60, 1440? ")
- #   if time != type(int):
- #       Raise
-    time = int(time)
-    print(f"File will be converted to a {time}-min datafile")
-    new_time = [item for item in freq if item == time]
+    try:
+        time = Prompt.ask("What frequency would you like to change the file to? [violet]15, 30, 60, 1440?[/violet] (or [red]'exit'[/red] to quit) ")
+        if time == 'exit':
+            sys.exit()
+        time = int(time)
+        if time < check:
+            raise Exception ("Cannot convert to a time lower than that of the file") 
+    except ValueError:
+        console.print(f"Please specify a valid time as an integer ({freq})", style='warning')
+        time_file()
 
     #STILL NEED TO FIND A WAY TO HANDLE COLUMNS WIIHOUT OBVIOUS HEADERS. I.E EVAPHR OR SIMPLE NAMING CONVENTIONS LIKE PRECIP 
-    avg_fil = df.filter(regex=r"vg$", axis=1)
-    max_fil = df.filter(regex=r"ax$", axis=1)
-    min_fil = df.filter(regex=r"in$", axis=1)
-    tot_fil = df.filter(regex=r"ot$", axis=1)
+    avg_fil = df.filter(regex=r"Avg$", axis=1)
+    max_fil = df.filter(regex=r"Max$", axis=1)
+    min_fil = df.filter(regex=r"Min$", axis=1)
+    tot_fil = df.filter(regex=r"Tot$", axis=1)
     win_fil = df.filter(regex=r"^W.*(?<![n,x])$", axis=1)
     sig_fil = df.filter(regex=r"^S.*(?<![n,x,g])$", axis=1)
     col_order = df.columns 
 #    check = time_check()
     try:    
-        if time < check:
-            pass
-        elif new_time[0] == int(15):
+        new_time = [item for item in freq if item == time]
+
+        if new_time[0] == int(15):
             freq = '15min'
         elif new_time[0] == int(30):
             freq = '30min'
@@ -94,48 +108,91 @@ def time_change():
             freq = 'h'
         elif new_time[0] == int(1440):
             freq = 'd'
+         
+    except Exception as e:
+        console.print(f"#2 Error occurred: {e}", style='error')
+        sys.exit()
+
+    df_avg = avg_fil.resample((freq), closed='right', label='right').mean()
+    df_max = max_fil.resample((freq), closed='right', label='right').mean()
+    df_min = min_fil.resample((freq), closed='right', label='right').mean()
+    df_win = win_fil.resample((freq), closed='right', label='right').mean()
+    df_sig = sig_fil.resample((freq), closed='right', label='right').mean()
+    df_tot = tot_fil.resample((freq), closed='right', label='right').sum()
         
-        df_avg = avg_fil.resample((freq), closed='right', label='right').mean()
-        df_max = max_fil.resample((freq), closed='right', label='right').mean()
-        df_min = min_fil.resample((freq), closed='right', label='right').mean()
-        df_win = win_fil.resample((freq), closed='right', label='right').mean()
-        df_sig = sig_fil.resample((freq), closed='right', label='right').mean()
-        df_tot = tot_fil.resample((freq), closed='right', label='right').sum()
-        
-        df_or  = pd.concat([df_avg,df_max,df_min,df_win,df_sig,df_tot], axis=1)
-        df_reordered = df_or[col_order] 
-        df_new = df_reordered.apply(lambda x: round(x,2))
-        df_new.dropna()
-        print(df_new)
-        return df_new, time
+    try: 
+        df_or = pd.concat([df_avg,df_max,df_min,df_win,df_sig,df_tot], axis=1)
+        df_list = df.columns.to_list()
+        df_or_list = df_or.columns.to_list()
+        df_list.extend(df_or_list)
+
+        if len(df_list) == len(df_or_list):
+            df_reor = df_or[col_order] 
+            df_new = df_reor.apply(lambda x: round(x,2))
+            return df_new, time
+
+"""lengthy way to handle columns not easily found via regex"""
+        elif len(df_list) != len(df_or_list):
+            df_list = list(set(df_list) ^ set(df_or_list))
+            console.print(f"Rows [green]{df_list}[/green] were not recognized.", style='warning')
+            user_input = {}
+            i = 0
+            while i < len(df_list): 
+                for item in df_list:
+                    input = Prompt.ask(f"What would you like to do for [bold green]{item}[/bold green]? Enter: Avg or Sum. ")
+                    value = input.lower()
+                    user_input.update({item:value})
+                    df_temp = df[item]
+                    df_new = pd.concat([df_temp])
+                    i+=1
+            val_avg = [key for key, val in user_input.items() if val == 'avg']
+            val_sum = [key for key, val in user_input.items() if val == 'sum']
+            df_avg = df[val_avg]
+            df_sum = df[val_sum]
+            df_cus = df_avg.resample((freq), closed='right', label='right').mean()
+            df_cus = df_sum.resample((freq), closed='right', label='right').sum()
+            df1= pd.concat([df_or,df_cus,df_avg])
+            df_reor = df1[col_order]
+            df_new = df_reor.apply(lambda x: round(x,2))
+            ic(df_new)
+            return df_new, time 
+        else:
+            raise 
+            sys.exit() 
+       # console.print(f"Success! File has been converted to a {time}-min datafile", style='info')
+    
+        #return df_new, time
 
     except Exception as e:
-        print(f"Error occurred: {e}")
+        console.print(f"#3 Error occurred: {e}", style='error')
+        sys.exit()
 
+        
 '''Export the new time file'''
 def time_file():
     date = datetime.datetime.now()
-    items = time_change()
     try: 
+        items = time_change()
         styled_df = items[0]
         time = items[1]
         styled_df.to_csv(date.strftime("%Y%m%d") + '-' f"{time}-min_{file_path}", index=True)
+        console.print(f"Success! File has been converted to a {time}-min datafile", style='info')
     
     except Exception as e:
-        print(f"Error occurred: {e}")
-
+        console.print(f"#4 Error occurred: {e}", style='error')
+        sys.exit()
 
 if __name__ == '__main__':
     while True:
-        file_path = input("Enter the path to the CSV file (or 'exit' to quit): ")
+        file_path = Prompt.ask("Enter the path to the CSV file (or [red]'exit'[/red] to quit): ")
    #     file_path = ("Met.csv") 
         if file_path == 'exit':
-            print("Exiting the program.")
+            rprint("Exiting the program.")
             sys.exit()
         elif file_path.endswith(".csv"):
             break  
         else:
-            print("This is not a CSV file. Please try again.")
+            console.print("This is not a CSV file. Please try again.", style='error')
 #    df = file_read(file_path)
 #    file_read(file_path)
 #    col_filter() 
