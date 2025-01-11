6406b280f679e5f0f2fd83e6d1ee7d2c3baf04a0
mrtumnis <thomas.hefferman1@gmail.com>
2025-01-10 17:19:39 -0700

    update SodarQC

--- QA_QC/SodarQC/sodar_QC.py
+++ QA_QC/SodarQC/sodar_QC.py
@@ -6,7 +6,6 @@ import pandas as pd
 import pyarrow as pa
 import sys 
 import datetime
-import timechange
 from xlsxwriter import Workbook
 from icecream import ic
 from rich import print as rprint
@@ -26,82 +25,79 @@ custom_theme = Theme({
 
 console = Console(theme=custom_theme)
 
-def pandas_read(file_path):
-    try:
-        df = pd.read_csv(file_path)#, dtype_backend='pyarrow')
-        for row in df.head(n=1).itertuples():
-            if any(isinstance(item, float) for item in row):
-                df = df.drop(0)
-        for col in df.columns:
-            if col == 'STATION':
-                df = df.drop('STATION', axis=1)
-        df1 = pd.to_datetime(df['TIMESTAMP'], errors='coerce')
-        df1 = df1.to_frame()
-        df = df.iloc[:,1:].astype(float)
-        df = pd.concat([df1, df], axis=1)
-        df.columns = df.columns.str.strip()
-       
-        return df 
+schema = {
+    'TIMESTAMP':'Datetime', 'VectorWindSpeed':'Float32', 'VectorWindDirection':'Int32', 'SpeedDirectionReliability':'Int32',
+    'W_Speed':'Float32', 'W_Reliability':'Int32', 'W_Count':'Int32', 'W_StandardDeviation':'Float32', 'W_Amplitude':'Int32', 'W_Noise':'Int32', 'W_SNR':'Float32', 'W_ValidCount':'Int32',
+    'V_Speed':'Float32', 'V_Reliability':'Int32', 'V_Count':'Int32', 'V_StandardDeviation':'Float32', 'V_Amplitude':'Int32', 'V_Noise':'Int32', 'V_SNR':'Float32', 'V_ValidCount':'Int32',
+    'U_Speed':'Float3', 'U_Reliability':'Int32','U_Count':'Int32', 'U_StandardDeviation':'Float32', 'U_Amplitude':'Int32', 'U_Noise':'Int32', 'U_SNR':'Float32', 'U_ValidCount':'Int32'
+}
 
-    except Exception as e:
-        console.print(f"#1 Error occurred: {e}", style='error')
-    return None
 
-def time_change():
-    df = pandas_read(file)
-    df = (
-        pl.from_pandas(df)
-    )
-    ic(type(df))
+def read_file(file_path):
+    schema = {
+        'TIMESTAMP':'Datetime', 'VectorWindSpeed':'Float32', 'VectorWindDirection':'Int32', 'SpeedDirectionReliability':'Int32',
+        'W_Speed':'Float32', 'W_Reliability':'Int32', 'W_Count':'Int32', 'W_StandardDeviation':'Float32', 'W_Amplitude':'Int32', 'W_Noise':'Int32', 'W_SNR':'Float32', 'W_ValidCount':'Int32',
+        'V_Speed':'Float32', 'V_Reliability':'Int32', 'V_Count':'Int32', 'V_StandardDeviation':'Float32', 'V_Amplitude':'Int32', 'V_Noise':'Int32', 'V_SNR':'Float32', 'V_ValidCount':'Int32',
+        'U_Speed':'Float3', 'U_Reliability':'Int32','U_Count':'Int32', 'U_StandardDeviation':'Float32', 'U_Amplitude':'Int32', 'U_Noise':'Int32', 'U_SNR':'Float32', 'U_ValidCount':'Int32'
+    }
 
-    #chagned time freq based on full dataframe, but it only kept the corresponding values in each row. 
-    df = (df.upsample(time_column='TIMESTAMP', every='1h')
-    .select(pl.all()
-    .forward_fill())
-    )
+#    try:
+       # file_lf = pl.scan_csv(file_path, has_header=True,try_parse_dates=True, skip_rows_after_header=1).collect()
+        #ic(file_lf)
+    df = pl.read_csv(file_path, schema=schema)#, dtype_backend='pyarrow')
+    ic(df)
+    # Drop first row due to string type 
+    index = [0]
+    df = df.filter(~pl.Series(range(len(df))).is_in(index))
+    ic(df)
 
-       
-    ic("Time", df)
+    return df 
 
+#    except Exception as e:
+#        console.print(f"#1 Error occurred: {e}", style='error')
+#    return None
+
+#return the proper interval and check for missing data
 def time_check():
-    df = pandas_read(file)
-    #    df = pl.DataFrame(df).lazy()
-    df = (
-        pl.from_pandas(df).lazy()
-    )
-
+    df = read_file(file)
     ic(type(df))
+    #    df = pl.DataFrame(df).lazy()
+    df = df.upsample(time='TIMESTAMP', every='15m')
+    df = pl.from_pandas(df).lazy()
+    df = df.fill_null(9999)
+    null = df.select(pl.col('TIMESTAMP').null_count())
+    null_pos = df.filter(pl.col('TIMESTAMP').is_null()).collect()
+    ic(null)
+    ic(null_pos)
     
-    df1 = (
-            df.group_by('TIMESTAMP', maintain_order = True)
-            .min()
-            .collect()
-    )
-    ic(type(df1))
 
-    return df1
+    return df
+
+
 
 def test():
- #   df = time_check()
-    df1 = time_change()
+    read_file(file)
+  #  df = time_check()
 #    df1 = pd.read_csv(file, dtype_backend='pyarrow')
-    rprint(df1)
+ #   rprint("time",df)
+    return None
+    
 
 def QAQC_file(file):
-    try:
-        date = datetime.datetime.now()
-        styled_df = test()
-        ic(styled_df)
-        styled_df.write_excel(date.strftime("%Y%m%d") + '-' + f"{file.strip('.csv')}" + '.xlsx')
+ #   try:
+    date = datetime.datetime.now()
+    styled_df = test()
+    ic(styled_df)
+    styled_df.write_excel(date.strftime("%Y%m%d") + '-' + f"{file.strip('.csv')}" + '.xlsx')
 
-    except Exception as e:
-        console.print(f"#1 Error occurred: {e}", style='error')
-    return None
+#    except Exception as e:
+#        console.print(f"#1 Error occurred: {e}", style='error')
+#    return None
 
 if __name__ == '__main__': 
     
 #file = input("What is the path to the .csv file ")
-    file = 'Met.csv'
+    file = 'SODAR.csv'
     QAQC_file(file)
 #    file_scan(file)
 #    file_read(file)
