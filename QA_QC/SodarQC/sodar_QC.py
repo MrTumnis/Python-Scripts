b02e548ff960a354371245b167c0bd46dcfaac85
mrtumnis <thomas.hefferman1@gmail.com>
2025-01-16 11:29:58 -0700

    start deviation check

--- QA_QC/SodarQC/sodar_QC.py
+++ QA_QC/SodarQC/sodar_QC.py
@@ -5,6 +5,7 @@ import polars.selectors as cs
 import pandas as pd
 import pyarrow as pa
 import sys 
+import logging
 import datetime
 import zipfile
 from icecream import ic
@@ -16,16 +17,21 @@ from rich.traceback import install
 
 install()
 
+logging.basicConfig(filename = 'sodarQC.log',
+                    format = '%(asctime)s %(message)s',
+                    filemode='w')
 
+
+logger = logging.getLogger(__name__)
 
 schema = {
     'TIMESTAMP':pl.Datetime ,'VectorWindSpeed':pl.Float32, 'VectorWindDirection':pl.Float32, 'SpeedDirectionReliability':pl.Float32,
-    'W_Speed':pl.Float32, 'W_Reliability':pl.Int32, 'W_Count':pl.Int32, 'W_StandardDeviation':pl.Float32,
-    'W_Amplitude':pl.Int32, 'W_Noise':pl.Int32, 'W_SNR':pl.Float32, 'W_ValidCount':pl.Int32,
-    'V_Speed':pl.Float32, 'V_Reliability':pl.Int32, 'V_Count':pl.Int32, 'V_StandardDeviation':pl.Float32,
-    'V_Amplitude':pl.Float32, 'V_Noise':pl.Int32, 'V_SNR':pl.Float32, 'V_ValidCount':pl.Int32,
-    'U_Speed':pl.Float32, 'U_Reliability':pl.Int32,'U_Count':pl.Int32, 'U_StandardDeviation':pl.Float32,
-    'U_Amplitude':pl.Int32, 'U_Noise':pl.Int32, 'U_SNR':pl.Float32, 'U_ValidCount':pl.Int32
+    'W_Speed':pl.Float32, 'W_Reliability':pl.Float32, 'W_Count':pl.UInt32, 'W_StandardDeviation':pl.Float32,
+    'W_Amplitude':pl.Int32, 'W_Noise':pl.Int32, 'W_SNR':pl.Float32, 'W_ValidCount':pl.UInt32,
+    'V_Speed':pl.Float32, 'V_Reliability':pl.Float32, 'V_Count':pl.UInt32, 'V_StandardDeviation':pl.Float32,
+    'V_Amplitude':pl.Float32, 'V_Noise':pl.Int32, 'V_SNR':pl.Float32, 'V_ValidCount':pl.UInt32,
+    'U_Speed':pl.Float32, 'U_Reliability':pl.Float32,'U_Count':pl.UInt32, 'U_StandardDeviation':pl.Float32,
+    'U_Amplitude':pl.Int32, 'U_Noise':pl.Int32, 'U_SNR':pl.Float32, 'U_ValidCount':pl.UInt32
 }
 
 
@@ -42,6 +48,7 @@ columns = {
 
 #Return all lazy files in a dictionary for easy reference and append height of each range gate to column name 
 def read_file(file_path, height=None) -> dict:
+
     null_items = ['TIMESTAMP', 'm/s', '\u00B0', ""]
 
     lazy_dict = {'30':'','35':'','40':'','45':'','50':'','55':'',
@@ -54,10 +61,13 @@ def read_file(file_path, height=None) -> dict:
             with zipfile.ZipFile(f'{file_path}', 'r') as zip_file:
                 file_path = file_path.strip('.zip')
                 zip_file.extractall(f'{file_path}')
-    except: 
-        raise 
 
-    else: 
+
+    except Exception as e:
+        logging.error(f"Error occured processing zip file {e}")
+        raise
+
+    try:
         for h in lazy_dict.keys():
             file = (
                 pl
@@ -85,90 +95,112 @@ def read_file(file_path, height=None) -> dict:
         else:
             return lazy_dict
 
+    except Exception as e:
+        logging.error(f"Error occured processing file {e}")
+
 #Merge dataframes with a single header starting with the 30m file
 def lf_merge() -> pl.LazyFrame:
     df_dic = read_file(file_path)  
     df_list = []
+
     for i in range(35,141,5):
        df_list.append(df_dic[str(i)])
+
     df = df_dic['30']
-#    i = 35
-#    while i < 140:
+
     for item in df_list:
         df = df.lazy().join(item.lazy(), on='TIMESTAMP', how='inner') 
-#            i+=5
+
     return df
 
 #return adjacent columns as lazyframes 
 def column_filter(col) -> list:
     lf = lf_merge() 
     adj_dic = {r:r+5 for r in range(35,136,5)}
-
-  #  i = 35
-  #  while i < 145:
     df_pairs = []
+    
     #create 2 column dataframes based on adjacent range gates
     for key, val in adj_dic.items():     
         ver_ws = lf.select(cs.by_name(f'{col}'+'_'+f'{key}',f'{col}'+'_'+f'{val}'))
         df_pairs.append(ver_ws)
-   #         i+=5
+
     return df_pairs
                  
 
-class Speed_Profile_Check:
-    def __init__(self, columns, threshold=None)
-        
-    self.columns = columns
-    self.threshold = 
-    self.validitycode = 9
 
 '''Begin Quality Checks'''
 #compare vertical('W') and horizontal('U/V')  wind speed at adjacent levels 
-def speed_profile_check() -> list:
+def speed_profile_check(col_list) -> list:
 
-    com_list = ['W_Speed', 'U_Speed', 'V_Speed']
-    
-    for col in com_list:
-        col_let = col.strip('_Speed')
+    lf_list = []
 
-        lf = column_filter(col)
-        lf_list = []
+    try:  
+        i = 0 
+        while i < len(col_list):
+            for col in col_list:
+                col_let = col.strip('_Speed')
+
+                lf = column_filter(col)
+                for item in lf:
+                    df = item 
+                    #return names as a list while maintaining lazyframe
+                    lf = df.collect_schema().names()
+                    col1 = lf[0]
+                    col2 = lf[1]
+
+                    if col1.startswith('W'):
+                        diff = 2
+                    else:
+                        diff = 5
+
+                    h = col1.strip(f'{col_let}' + 'Speed_')
+                    
+                    '''perform a difference check on the adjacent Speed range gates and return a 9 if pass, and a 2 if fail'''
+                    df_col= df.with_columns(
+                        (pl.when(
+                                pl.col(col1).abs() - 
+                                pl.col(col2).abs() >= (diff))
+                            .then(2)
+                            .otherwise(9)
+                        ).alias(f'{col_let}'+'_Reliability_' + f'{h}')
+                        .cast(pl.UInt32)
+                    )
+                    
+                    df = df_col
+                    lf_list.append(df) 
+            i+=1
+
+            return lf_list 
+
+    except Exception as e:
+        logging.error(f"Error performing speed_profile_check {e}")
+
+def standard_dev_check():
+    lf = column_filter(col)
+    for item in lf:
+        df = item 
+        df_col= df.with_columns(
+            (pl.when(
+                    pl.col(col1).abs() - 
+                    pl.col(col2).abs() >= (diff))
+                .then(2)
+                .otherwise(9)
+            ).alias(f'{col_let}'+'_Reliability_' + f'{h}')
+            .cast(pl.UInt32)
+        )
+                    
+        
 
 
-        for item in lf:
-            df = item 
-            #return names as a list while maintaining lazyframe
-            lf = df.collect_schema().names()
-            col1 = lf[0]
-            col2 = lf[1]
-            height = [col1.strip(f'{col_let}' + 'Speed_') for col in col1]
-            h = height[0]
-            #Materialize lazyframe to perform column comparison
-            df = df.collect()
-            
-            '''perform a difference check on the adjacent vertical ('W' columns) speed range gates and return a 9 if pass, and a 2 if fail'''
-            df_col= df.with_columns(
-                (pl.when(
-                        pl.col(col1).abs() - 
-                        pl.col(col2).abs() >= 2)
-                    .then(2)
-                    .otherwise(9)
-                ).alias(f'{col_let}'+'_Reliability_' + f'{h}')
-            )
-
-            #change back to lazyframe
-            df = df_col.lazy()
-            lf_list.append(df) 
-            ic(len(lf_list))
-        for i in lf_list:
-            ic(i.collect())
-
-        return df
 
 
 
 def test():
-    df = component_speed_profile_check()
+    vec_list = ['VectorWindSpeed']
+    com_list = ['W_Speed', 'U_Speed', 'V_Speed']
+    df = speed_profile_check(vec_list)
+    for lf in df:
+        ic(lf.collect())
 #    df = column_filter('U_Speed')
   #  df = df[0]
    # ic(df)
@@ -194,5 +226,3 @@ if __name__ == '__main__':
     file_path = 'GPWauna_data.zip'
 #    QAQC_file()
     test()
-#    file_scan(file)
-#    file_read(file)
