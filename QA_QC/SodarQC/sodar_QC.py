46e17ed945d1566b0579165ab76188e9813a05ec
mrtumnis <thomas.hefferman1@gmail.com>
2025-01-21 19:02:25 -0700

    finish check functions

--- QA_QC/SodarQC/sodar_QC.py
+++ QA_QC/SodarQC/sodar_QC.py
@@ -2,17 +2,11 @@
 
 import polars as pl
 import polars.selectors as cs
-import pandas as pd
-import pyarrow as pa
-import sys 
 import logging
 import datetime
 import zipfile
 from icecream import ic
 from rich import print as print
-from rich.console import Console
-from rich.theme import Theme
-from rich.prompt import Prompt
 from rich.traceback import install
 
 install()
@@ -28,11 +22,11 @@ logger = logging.getLogger(__name__)
 schema = {
     'TIMESTAMP':pl.Datetime ,'VectorWindSpeed':pl.Float32, 'VectorWindDirection':pl.Float32, 'SpeedDirectionReliability':pl.Float32,
     'W_Speed':pl.Float32, 'W_Reliability':pl.Float32, 'W_Count':pl.Float32, 'W_StandardDeviation':pl.Float32,
-    'W_Amplitude':pl.Float32, 'W_Noise':pl.Float32, 'W_SNR':pl.Float32, 'W_ValidCount':pl.Float32,
+    'W_Amplitude':pl.Int32, 'W_Noise':pl.Float32, 'W_SNR':pl.Float32, 'W_ValidCount':pl.Float32,
     'V_Speed':pl.Float32, 'V_Reliability':pl.Float32, 'V_Count':pl.Float32, 'V_StandardDeviation':pl.Float32,
-    'V_Amplitude':pl.Float32, 'V_Noise':pl.Float32, 'V_SNR':pl.Float32, 'V_ValidCount':pl.Float32,
+    'V_Amplitude':pl.Int32, 'V_Noise':pl.Float32, 'V_SNR':pl.Float32, 'V_ValidCount':pl.Float32,
     'U_Speed':pl.Float32, 'U_Reliability':pl.Float32,'U_Count':pl.Float32, 'U_StandardDeviation':pl.Float32,
-    'U_Amplitude':pl.Float32, 'U_Noise':pl.Float32, 'U_SNR':pl.Float32, 'U_ValidCount':pl.Float32
+    'U_Amplitude':pl.Int32, 'U_Noise':pl.Float32, 'U_SNR':pl.Float32, 'U_ValidCount':pl.Float32
 }
 
 
@@ -74,8 +68,7 @@ def read_file(file_path, height=None) -> dict:
             file = (
                 pl
                 .scan_csv(f'{file_path}/Wauna_SODAR{h}_Table15.csv', has_header=True, null_values=null_items, raise_if_empty=True)
-                .with_columns(
-                    pl
+                .with_columns(pl
                     .col('TIMESTAMP').str
                     .to_datetime('%Y-%m-%d %H:%M:%S', strict=False)).cast(schema)
             )
@@ -90,7 +83,7 @@ def read_file(file_path, height=None) -> dict:
             lf = df.lazy()
             lazy_dict.update({key:lf})
 
-        #Return a single lazy frame based on height of data recordings 
+        #Return a lazy frame based on height of data recordings 
         if height is not None:
             lf = lazy_dict[height]
             return lf 
@@ -122,122 +115,174 @@ def lf_merge() -> pl.LazyFrame:
 
 
 
-#return adjacent columns as lazyframes, or a dataframe of 3 columns in the same range gate
-def column_filter(col=None, col_series=None):
-    lf = lf_merge() 
-#    try:
-    if col is not None:
-        adj_dict = {r:r+5 for r in range(35,136,5)}
-        df_pairs = []
+def speed_profile_check() -> list:
+    lf = lf_merge()
+    speed_list = []
+    h = 30
+    h2 = h + 5
+    
+    '''perform a difference check on the adjacent range gates and return a 9 if pass, and a 2 to flag '''
+    while h < 145: 
+        w_df = lf.select(pl
+                         .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & (pl.col(f'W_Speed_{h}') - pl.col(f'W_Speed_{h2}').abs() >= 2))
+                         .then(2)
+                         .otherwise(9)
+                         .alias(f'W_Speed_Check_{h}')).cast(pl.UInt32)
         
-        #create 2 column dataframes based on adjacent range gates
-        for key, val in adj_dict.items():     
-            ver_ws = lf.select(cs.by_name(f'{col}_{key}',f'{col}_{val}'))
-            df_pairs.append(ver_ws)
-            ic('Test')
+        speed_list.append(w_df)
+        u_df = lf.select(pl
+                        .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & (pl.col(f'U_Speed_{h}') - pl.col(f'U_Speed_{h2}').abs() >= 2))
+                        .then(2)
+                        .otherwise(9)
+                        .alias(f'U_Speed_Check_{h}')).cast(pl.UInt32)
 
-        return df_pairs
-                 
+        speed_list.append(u_df)
+        v_df = lf.select(pl
+                       .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & (pl.col(f'V_Speed_{h}') - pl.col(f'V_Speed_{h2}').abs() >= 2))
+                       .then(2)
+                       .otherwise(9)
+                       .alias(f'V_Speed_Check_{h}')).cast(pl.UInt32)
+        
+        speed_list.append(v_df)
+        vec_df = lf.select(pl
+                       .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & (pl.col(f'VectorWindSpeed_{h}') - pl.col(f'VectorWindSpeed_{h2}').abs() >= 5))
+                       .then(2)
+                       .otherwise(9)
+                       .alias(f'VectorWindSpeed_Check_{h}')).cast(pl.UInt32)
+        
+        speed_list.append(vec_df)
+        h +=5 
+    return speed_list
+    
 
-    '''Used for testing. May need to move this inside of std dev check function'''
-    if col_series is not None:
-        series = [] 
-        i = 30
-        while i < 145:
-            ic(i)
-            lf = lf_merge() 
-            height.append(i)
-            lf = lf.select(cs.by_name(f'W_StandardDeviation_{i}', f'U_StandardDeviation_{i}', f'V_StandardDeviation_{i}'))
-            series.append(lf)
-            i+=5
-
-    return None
-
-
-def speed_profile_check(col_list, diff) -> list:
-
+def standard_dev_check() -> list:
+    lf = lf_merge()
     lf_list = []
-
-    try:  
-        i = 0 
-        while i < len(col_list):
-            for col in col_list:
-                col_letter = col.strip('_Speed')
-
-                lf = column_filter(col)
-                for item in lf:
-                    df = item 
-                    #return names as a list while maintaining lazyframe
-                    lf = df.collect_schema().names()
-                    col1 = lf[0]
-                    col2 = lf[1]
-
-                    h = col1.strip(f'{col_letter}Speed_')
-                    
-                    '''perform a difference check on the adjacent range gates and return a 9 if pass, and a 2 to flag '''
-                    df_col= df.with_columns(
-                        (pl.when(
-                                pl.col(col1).abs() - 
-                                pl.col(col2).abs() >= (diff))
-                            .then(2)
-                            .otherwise(9)
-                        ).alias(f'{col_letter}_Reliability_{h}')
-                        .cast(pl.UInt32)
+    try:
+        h = 30
+        while h < 145:          
+            condition = (
+                   pl.col(f'W_StandardDeviation_{h}') > 1) | (
+                       pl.col(f'U_StandardDeviation_{h}') + pl.col(f'W_StandardDeviation_{h}') > 5) | (
+                       pl.col(f'V_StandardDeviation_{h}') / pl.col(f'U_StandardDeviation_{h}') > 5)
+                
+            df = lf.select(
+                        pl.when(condition)
+                        .then(2)
+                        .otherwise(9)
+                        .alias(f'STD_Reliability_{h}')
+                         .cast(pl.UInt32)
                     )
-                    
-                    df = df_col
-                    lf_list.append(df) 
-            i+=1
+            
+            h += 5
+            lf_list.append(df)
 
-            return lf_list 
+        return lf_list 
 
     except Exception as e:
-         logging.error(f"Error performing speed_profile_check {e}")
+        logging.error(f"Error performing standard deviation check {e}")
 
-
-def standard_dev_check():
+def noise_check() -> list:
     lf = lf_merge()
-    lf_col = lf.collect_schema().names()
-    lf_list = []
-    h = 30
-    while h < 145:          
-        condition = (
-               pl.col(f'W_StandardDeviation_{h}') > 1) | (
-                   pl.col(f'U_StandardDeviation_{h}') + pl.col(f'W_StandardDeviation_{h}') > 5) | (
-                   pl.col(f'V_StandardDeviation_{h}') / pl.col(f'U_StandardDeviation_{h}') > 5)
-            
-        df = lf.select(
-                    pl.when(condition)
-                    .then(2)
-                    .otherwise(9)
-                    .alias(f'std_reliability_{h}')
-                     .cast(pl.UInt32)
-                )
-        
-        h += 5
+    noise_list = []
+    try:
+        h = 30
+        h2 = h + 5
+        while h < 145: 
+            con_list = [(pl.col(f'W_Amplitude_{h}') < pl.col(f'W_Amplitude_{h2}')) & (
+                    pl.col(f'VectorWindSpeed_{h}') > pl.col(f'VectorWindSpeed_{h2}')), 
+                           (pl.col(f'U_Amplitude_{h}') < pl.col(f'U_Amplitude_{h2}')) & (
+                    pl.col(f'VectorWindSpeed_{h}') > pl.col(f'VectorWindSpeed_{h2}')), 
+                           (pl.col(f'V_Amplitude_{h}') < pl.col(f'V_Amplitude_{h2}')) & (
+                    pl.col(f'VectorWindSpeed_{h}') > pl.col(f'VectorWindSpeed_{h2}'))]
 
-        lf_list.append(df)
-    ic(lf_list)
-    return lf_list 
+            for condition in con_list:        
+                dr = lf.select(pl
+                               .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & condition)
+                               .then(2)
+                               .otherwise(9)
+                               .alias(f'Noise_Check_{h}')
+                               ).cast(pl.UInt32)
 
-        # except Exception as e:
-        #     logging.error(f"Error performing standard deviation check {e}")
+            noise_list.append(dr)
+            h +=5 
+           # lf = pl.join(df)
+        return noise_list
 
-def df_concat() -> pl.DataFrame:
-#    vec_list = ['VectorWindSpeed']
-#    com_list = ['W_Speed', 'U_Speed', 'V_Speed']
-#    vector_df = speed_profile_check(vec_list, diff = 5)
-#    com_df = speed_profile_check(vec_list, diff = 2)
+    except Exception as e:
+        logging.error(f"Error performing noise check {e}")
+
+def echo_check() -> list:
+    lf = lf_merge()
+    echo_list = []
+    try:
+        h = 30
+        h2 = h + 5
+        while h < 145: 
+            con_list = [(pl.col(f'W_Amplitude_{h}') < pl.col(f'W_Amplitude_{h2}')) & (
+                    pl.col(f'W_Speed_{h}') > pl.col(f'W_Speed_{h2}')), 
+                           (pl.col(f'U_Amplitude_{h}') < pl.col(f'U_Amplitude_{h2}')) & (
+                    pl.col(f'U_Speed_{h}') > pl.col(f'U_Speed_{h2}')), 
+                           (pl.col(f'V_Amplitude_{h}') < pl.col(f'V_Amplitude_{h2}')) & (
+                    pl.col(f'V_Speed_{h}') > pl.col(f'V_Speed_{h2}'))]
+
+            for condition in con_list:        
+                df = lf.select(pl
+                               .when(pl.col('TIMESTAMP').dt.hour().is_between(10,17) & condition)
+                               .then(2)
+                               .otherwise(9)
+                               .alias(f'Echo_Check_{h}')
+                               ).cast(pl.UInt32)
+
+            echo_list.append(df)
+            h +=5 
+
+        return echo_list 
+
+    except Exception as e:
+        logging.error(f"Error performing echo check {e}")
+
+def precip_check():
+    lf = lf_merge()
+    precip_list = []
+    try:
+        h = 30
+        while h < 145:
+            df = lf.select(pl
+                           .when(pl.col(f'W_Speed_{h}') < -3)
+                           .then(3)
+                           .otherwise(9)
+                           .alias(f'Precip_Check_{h}')
+                           ).cast(pl.UInt32)
+            h += 5
+            precip_list.append(df)
+
+        return precip_list 
+    except Exception as e:
+        logging.error(f"Error performing echo check {e}")
+
+
+#def echo_check(): could add later, but not recommended for QA/QC
     
-    std_df = standard_dev_check()
-    ic(std_df)
-    return None 
+
+
+#merge all QC columns into one Dataframe and compare the validity of each range gate, then return the lowest number as the valid code. 
+def df_merge() -> pl.DataFrame:
+    lf1 = pl.concat(speed_profile_check(), how='horizontal', parallel=True)
+    lf2 = pl.concat(standard_dev_check(), how='horizontal', parallel=True)
+    lf3 = pl.concat(noise_check(), how='horizontal', parallel=True)
+    lf4 = pl.concat(echo_check(), how='horizontal', parallel=True)
+    lf5 = pl.concat(precip_check(), how='horizontal', parallel=True)
+    df1 = pl.concat([lf1,lf2,lf3,lf4,lf5], how='horizontal')
+    ic(df1.collect())
+
+    return df1 
 
 def QAQC_file():
 #    try:
     date = datetime.datetime.now()
-    styled_df = df_concat()
-    styled_df.write_csv(date.strftime("%Y%m%d") + '-' + 'SODAR_QA-QC' + '.csv', include_header=True)
+    merged_df = df_merge()
+    merged_df.write_csv(date.strftime("%Y%m%d") + '-' + 'SODAR_QA-QC' + '.csv', include_header=True)
    # styled_df.write_excel(date.strftime("%Y%m%d") + '-' + 'SODAR_df' + '.xlsx')
  #   except Exception as e:
  #       logging.error(f"Error writing to csv {e}")
